7e6278147cc1961c92c94b225a2cb8536a8af2d7
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2019-02-23 11:36:00 -0400

    Add initial pages

---
+++ src/Pages/Error404Page/Error404Page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error404Page = () => (
+  <div className="App">
+    <div className="Content--fullWidth">
+      <div>There's nothing here</div>
+    </div>
+  </div>
+);
+
+export default Error404Page;
