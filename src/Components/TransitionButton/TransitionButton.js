006598393d8a57416e06c2d2acbeb2a8ca4d6698
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2019-02-24 01:54:25 -0400

    refactor transition button

---
+++ src/Components/TransitionButton/TransitionButton.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const transition = `transition: 0.3s background-color ease-in-out, 0.3s box-shadow ease-in-out;`;
+
+export const TransitionButton = styled.button`
+  ${transition}
+`;
