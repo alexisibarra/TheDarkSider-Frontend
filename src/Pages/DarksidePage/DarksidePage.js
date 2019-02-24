29fbbab36007d6dbcd005572bbf621da2fb38ff3
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2019-02-24 03:13:11 -0400

    refactor

--- src/Pages/DarksidePage/DarksidePage.js
+++ src/Pages/DarksidePage/DarksidePage.js
@@ -1,38 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { routes } from "../../config/Routing/routesLiterals";
 import { logout } from "../../Ducks/AuthReducer/AuthReducer";
 
-class DarksidePage extends Component {
-  componentWillMount() {
-    console.log("hello world!");
-  }
-
-  render() {
-    const { history, logout } = this.props;
-    return (
-      <div>
-        DarkSide
-        <p>
-          <button
-            className="p-4 text-white underline"
-            onClick={async _ => {
-              await logout();
-              history.push(routes.signin);
-            }}
-          >
-            Logout
-          </button>
-        </p>
-      </div>
-    );
-  }
-}
+const DarksidePage = ({ history, logout }) => (
+  <div>
+    DarkSide
+    <p>
+      <button
+        className="p-4 text-white underline"
+        onClick={async _ => {
+          await logout();
+          history.push(routes.signin);
+        }}
+      >
+        Logout
+      </button>
+    </p>
+  </div>
+);
 
 DarksidePage.propTypes = {
-  prop: PropTypes.object
+  prop: PropTypes.object,
+  logout: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 const mS = state => ({});
